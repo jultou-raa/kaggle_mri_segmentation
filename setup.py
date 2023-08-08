bb60b7d4ea343da7b3d5571cbf4fcc0be63ab936
jultou-raa <jultou@gmail.com>
2023-08-08 14:24:11 +0200

    Update setup files

--- setup.py
+++ setup.py
@@ -5,20 +5,6 @@ setup(
     name='demo',
     version='0.1.0',
     packages=find_packages(),
-    install_requires=[
-        'albumentations',
-        'natsort',
-        'numpy',
-        'pandas',
-        'opencv-python-headless',
-        'plotly',
-        'lightning',
-        'scikit-learn',
-        'streamlit',
-        'torch',
-        'torchmetrics',
-        'torchvision',
-    ],
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
@@ -26,11 +12,4 @@ setup(
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
     ],
-    # Added deployment requirements
-    setup_requires=[
-        'pytest',
-        'pytest-cov',
-        'pytest-sugar',
-        'pylint'
-    ]
 )
